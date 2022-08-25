be1728a86977156b8b1af27cc007097c647fec2d
Mwenje <mwenje24@gmail.com>
2022-08-25 13:10:46 +0300

    saved work

--- Rakefile
+++ Rakefile
@@ -3,6 +3,27 @@ require 'sinatra/activerecord/rake'
 
 # CREATE TASK WITHOUT DESCRIPTION
 
+task :hello_world do
+    puts "Hello world"
+end
+
 # CREATE TASK WITH DESCRIPTION
+desc 'Find .01% of 14M'
+task :complex_math do
+    output = (0.01/100) * 14000000
+    puts output
+end
 
 # ORGANIZE TASKS IN NAMESPACES
+namespace :math_concepts do
+    desc 'addition'
+    task :add, [:num1, :num2] do
+        sum = num1 + num2
+        puts "Addition is #{sum}"
+    end
+
+    desc 'subtraction'
+    task :subtract do
+        puts "subtraction"
+    end
+end
--- app/app.rb
+++ app/app.rb
@@ -2,10 +2,24 @@ require "pry"
 require "active_record"
 
 # TODO: CONNECT TO THE DATABASE
+ActiveRecord::Base.establish_connection(
+    adapter: "sqlite3",
+    database:"db/school.db"
+)
 
 # TODO: CREATE TABLE
+create_courses_table = <<-SQL
+    CREATE TABLE IF NOT EXISTS courses(
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        description TEXT,
+        duration INTEGER )
+    SQL
+ActiveRecord::Base.connection.execute(create_courses_table)
 
 # TODO: Create class that associates with the db table
+class Course < ActiveRecord::Base
+end
 
 # TODO: VIEW METHODS AVAILABLE FOR TABLE (methods)
 
@@ -20,3 +34,5 @@ require "active_record"
 # TODO: FIND BY GIVEN CONDITIONS (find_by)
 
 # TODO: UPDATE RECORDS (save)
+
+# binding.pry
